492645b1a4bff8ce6fa53c525a426da2ac3290fa
webobscure <sparkygino@gmail.com>
2023-04-02 13:24:02 +0300

    create navbar

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+export {default as home } from './home.png';
+export {default as cart } from './cart.png';
+export {default as promo } from './promo.png';
+export {default as diagram } from './diagram.png';
+export {default as wallet } from './wallet.png';
+export {default as avatar } from './avatar.png';
+export {default as service } from './service.png';
+export {default as logout } from './logout.png';
---
+++ src/components/Header.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+import { home, cart, promo, diagram, wallet, avatar, service, logout } from '../assets';
+const Header = () => {
+
+  return (
+    <div>
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="/" className='logo'>
+              <img src={logo} alt="logo" className='icon-logo' />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={home} alt="" className="icons"/>
+              <span className="nav-item">Главная</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={cart} alt="" className="icons"/>
+              <span className="nav-item">Мои магазины</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={promo} alt="" className="icons"/>
+              <span className="nav-item">Промоматериалы</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={diagram} alt="" className="icons"/>
+              <span className="nav-item">Статистика</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={wallet} alt="" className="icons"/>
+              <span className="nav-item">Баланс</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={avatar} alt="" className="icons"/>
+              <span className="nav-item">Персональные данные</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={service} alt="" className="icons"/>
+              <span className="nav-item">Техподдержка</span>
+            </a>
+          </li>
+          <li>
+            <a href="/" className='logout'>
+              <img src={logout} alt="" className="icons"/>
+              <span className="nav-item">Выход</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
